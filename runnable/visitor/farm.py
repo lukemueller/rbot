f247c8498832b9ba2e9123b93f6adc0a14768fd4
Luke Mueller <luke.jordan.mueller@gmail.com>
2013-03-09 12:53:14 -0700

    Removed runnable dir.

--- runnable/visitor/farm.py
+++
@@ -1,9 +0,0 @@
-from runnable.base_runnable import BaseRunnable
-
-
-class Farm(BaseRunnable):
-
-    def run(self):
-        self.do_action("target")
-        self.do_action_and_pause("attack", 5)
-        self.spam_pickup(2)
