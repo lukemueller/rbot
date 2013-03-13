747259a097c54969a3f8ff139155b057a4ba7851
Luke Mueller <luke.jordan.mueller@gmail.com>
2013-03-13 00:24:32 -0600

    Modified runnable class.

--- lib/runnable.py
+++ lib/runnable.py
@@ -3,22 +3,22 @@ from time import time
 
 class Runnable():
 
-    def __init__(self, skill_bar, task):
-        self._task = task
-        self._skill_bar = skill_bar
+    def __init__(self, cycle_time, priority, actions):
+        self.cycle_time = int(cycle_time)
+        self.priority = priority
+        self._actions = actions
         self._last_run = None
 
-    def initialize(self):
-        self._skill_bar.initialize_with_task(self._task)
-
     def should_run(self):
+        if self._last_run is None:
+            return True
+
         time_since_last_run = time() - self._last_run
-        return time_since_last_run > int(self._task.cycle_time)
+        return time_since_last_run > self.cycle_time
 
-    def run(self):
+    def run(self, skillbar):
+        for action in self._actions:
+            # do action through skillbar API
+            return None
+
         self._last_run = time()
-        for tuple in self._task.actions:
-            self._skill_bar.do_action(tuple[0])
-
-    def get_priority(self):
-        return self._task.priority
