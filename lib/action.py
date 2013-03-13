bba5a32969c10aa15979be98ec1db670996fcc31
Luke Mueller <luke.jordan.mueller@gmail.com>
2013-03-12 23:55:01 -0600

    Added runnable factory to generate actions for runnable from config. Modified action class for runnable factory. Removed config class."

--- lib/action.py
+++ lib/action.py
@@ -1,18 +1,17 @@
 from autopy import key
 
+
 class Action():
 
-    KEY_MAP = { 'F1':key.K_F1, 'F2':key.K_F2, 'F3':key.K_F3, 'F4':key.K_F4, 'F5':key.K_F5, 'F6':key.K_F6,
-                'F7':key.K_F7, 'F8':key.K_F8, 'F9':120, 'F10':key.K_F10, 'F11':key.K_F11, 'F12':key.K_F12
-    }
+    KEY_MAP = {'F1': key.K_F1, 'F2': key.K_F2, 'F3': key.K_F3, 'F4': key.K_F4, 'F5': key.K_F5, 'F6': key.K_F6,
+               'F7': key.K_F7, 'F8': key.K_F8, 'F9': 120, 'F10': key.K_F10, 'F11': key.K_F11, 'F12': key.K_F12}
 
-
-    def __init__(self, name, coords, preferred_method='key', pause=None):
+    def __init__(self, name, raw_key, coords, pause=None, preferred_method='key'):
         self.name = name
         self.coords = coords
-        self.key_binding = None
+        self.key_binding = self._map_key_binding(raw_key)
         self.preferred_method = preferred_method
         self.pause = pause
 
-    def set_key_binding(self, key):
+    def _map_key_binding(self, key):
         self.key_binding = Action.KEY_MAP[key]
---
+++ util/runnable_factory.py
@@ -0,0 +1,58 @@
+from os import path, sep
+from lib.action import Action
+from lib.runnable import Runnable
+
+
+class RunnableFactory():
+
+    def __init__(self, role, task):
+        self.role = role
+        self.task = task
+        self._config_path = self._get_config_path()
+
+    def generate_runnable(self):
+        line_groups = self._get_grouped_lines()
+        runnables = []
+
+        for group in line_groups:
+            cycle_time, priority = self._get_cycle_time_and_priority_from_line_group_in_config(group)
+            actions = []
+            for line in group[2:]:
+                actions.append(self._generate_action_from_line_in_config(line))
+
+            runnables.append(Runnable(cycle_time, priority, actions))
+
+        return runnables
+
+    def _get_cycle_time_and_priority_from_line_group_in_config(self, line_group):
+        return (line_group[0], line_group[1])
+
+    def _generate_action_from_line_in_config(self, line):
+        action_name, key, pause = line.rstrip().split(' ')
+        return Action(action_name, key, None, pause)
+
+    def _get_config_path(self):
+        relative_path = 'config' + sep + self.role + sep + self.task + '.cfg'
+        return path.abspath(relative_path)
+
+    def _get_grouped_lines(self):
+        config_lines = self._read_config_file()
+        grouped_lines = []
+
+        there_are_still_tasks = True
+        while there_are_still_tasks:
+            try:
+                index = config_lines.index('\n')
+                grouped_lines.append(config_lines[:index])
+                config_lines = config_lines[index+1:]
+            except ValueError:
+                there_are_still_tasks = False
+
+        return grouped_lines
+
+    def _read_config_file(self):
+        config = open(self._config_path, 'r')
+        lines = config.readlines()
+        config.close()
+
+        return lines
